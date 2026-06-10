a9763812e4517e4d879f85a1a0644e17507a9590
thegarbzinvasion <peteralisson123@gmail.com>
2026-06-10 15:20:04 +0800

    Defining Rules and Rewards

--- knowledge-base.py
+++ knowledge-base.py
@@ -1,9 +1,7 @@
-import queue 
-
 class KnowledgeBase():
     def __init__(self):
-        ## Passanger State represented as a list of 5 elements, each element can be 0, 1, 2, 3, or 4
-        self.state_labels = {
+        ## Passenger State represented as a list of 5 elements, each element can be 0, 1, 2, 3, or 4
+        self.passenger_states = {
             '0': 'Red',
             '1': 'Green',
             '2': 'Yellow',
@@ -11,16 +9,16 @@ class KnowledgeBase():
             '4': 'In taxi',
         }
 
-        self.state_labels = {
-            ## Drop-off Locations represented as a list of 4 elements, each element can be 0, 1, 2, or 3
+        ## Drop-off Locations represented as a list of 4 elements, each element can be 0, 1, 2, or 3
+        self.drop_off_locations = {
             '0': 'Red',
             '1': 'Green',
             '2': 'Yellow',
             '3': 'Blue', 
         }
 
-        self.state_labels = {
-            ## Available Actions for the Taxi represented as a list of 6 elements, each element can be 0, 1, 2, 3, 4, or 5
+        ## Available Actions for the Taxi represented as a list of 6 elements, each element can be 0, 1, 2, 3, 4, or 5
+        self.available_actions = {
             '0': 'Move south (down)',
             '1': 'Move north (up)',
             '2': 'Move east (right)',
@@ -28,4 +26,14 @@ class KnowledgeBase():
             '4': 'Pick up passenger',
             '5': 'Drop off passenger',
         }
+
+        ## Rewards & Penalties for the Taxi Environment
+        self.goal_rewards = {
+            'successful_drop_off': 20,
+            'illegal_pick_up': -10,
+            'illegal_drop_off': -10,
+            'action_cost': -1,
+        }
+        
+        
 
